f72fd9c28f9026586f9fe6a503f030d0a0e7c699
Adriano <adrianosantosr1@gmail.com>
2022-12-08 19:40:12 -0300

    adicionando rota details

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./global.css";
 import Home from "./pages/home";
+import Details from "./pages/details";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/details/:id" element={<Details />} />
+    </Routes>
+  </Router>
+);
