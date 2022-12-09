a07a1d5a8814623807fa74ba842f8d201e156229
Adriano <adrianosantosr1@gmail.com>
2022-12-09 00:19:09 -0300

    alterando titulo do h1

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -20,7 +20,7 @@ const Home = () => {
 
   return (
     <Container>
-      <h1>Movies</h1>
+      <h1>Últimos lançamentos</h1>
       <MovieList>
         {movies.map((movie) => {
           return (
