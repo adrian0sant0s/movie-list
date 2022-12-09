48aac185a82c44b501744800eb9d2d5dadf94425
Adriano <adrianosantosr1@gmail.com>
2022-12-08 22:41:44 -0300

    adicionando dados da api e estilizandos

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,9 +1,47 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { ApiKey } from "../../config/key";
+import { Container } from "./styles";
 
 const Details = () => {
   const { id } = useParams();
-  console.log(id);
-  return <h1>hello world</h1>;
+  const [movie, setMovie] = useState({});
+  console.log(movie);
+  const imageMovie = "https://image.tmdb.org/t/p/w500/";
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${ApiKey}&language=pt-BR`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const movie = {
+          id,
+          title: data.title,
+          sinopse: data.overview,
+          image: `${imageMovie}${data.poster_path}`,
+          realeseDate: data.release_date,
+        };
+
+        setMovie(movie);
+      });
+  }, [id]);
+
+  return (
+    <Container>
+      <div className="container-movie">
+        <img src={`${movie.image}`} alt={movie.title} />
+        <div className="details">
+          <h1>{movie.title}</h1>
+          <span>Sinopse: {movie.sinopse}</span>
+          <span className="realeaseDate">
+            Data de lançamento: {movie.realeseDate}
+          </span>
+          <button>Voltar</button>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Details;
---
+++ src/pages/details/styles.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 4rem 0;
+
+  .container-movie {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .details {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 4rem;
+    max-width: 50%;
+  }
+
+  img {
+    width: 300px;
+    border-radius: 1rem;
+  }
+
+  h1 {
+    margin: 3rem 0;
+  }
+
+  span {
+    line-height: 30px;
+    margin-bottom: 15px;
+  }
+
+  .realeaseDate {
+    opacity: 0.5;
+  }
+
+  button {
+    background-image: linear-gradient(to bottom, #008080, #000);
+    color: #eee;
+    border-radius: 15px;
+    font-size: 1rem;
+    padding: 0.6rem 1.2rem;
+    border: none;
+    margin-top: 15px;
+    cursor: pointer;
+  }
+
+  button:hover {
+    opacity: 0.8;
+  }
+
+  button:active {
+    opacity: 1;
+  }
+`;
